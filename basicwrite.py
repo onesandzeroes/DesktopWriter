b9f568b19e45a2363cc0cc7a767b7f599c7d38c0
Marius Mather <marius.mather@gmail.com>
2012-05-08 19:10:29 +1000

    Cleaned up the populate_keys() method

--- basicwrite.py
+++ basicwrite.py
@@ -16,8 +16,7 @@ class DesktopFile:
                   'Categories', 'Keywords', 'StartupNotify' 'StartupNotify',
                   'StartupWMClass', 'URL']
     def __init__(self, filename):
-        self.read_in = open(filename, 'r')
-        self.all_list = [line for line in self.read_in]
+        self.old = open(filename, 'r')
         self.entry_dict = {}
         self.populate_keys()
     def edit_key(self, key, value):
@@ -27,12 +26,13 @@ class DesktopFile:
     def find_current_key(self, key):
         pass
     def populate_keys(self):
-        for line in self.all_list:
-            if self.key_line.match(line):
-                split_line = line.split('=')
-                current_key = split_line[0]
-                current_val = split_line[1]
-                self.entry_dict[current_key] = current_val
+        # List comprehension to find all lines containing a key=value pair
+        key_lines = [line for line in self.old if self.key_line.match(line)]
+        for line in key_lines:
+            split_line = line.split('=')
+            current_key = split_line[0]
+            current_val = split_line[1].rstrip()
+            self.entry_dict[current_key] = current_val
 
 if __name__ == '__main__':
     des = DesktopFile('desuratest.desktop')
