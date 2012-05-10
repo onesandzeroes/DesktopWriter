c013f2f8ad122d30cbed4d6f2e942da04b1b9fb3
Marius Mather <marius.mather@gmail.com>
2012-05-10 19:41:07 +1000

    Split the methods to read existing files off into a subclass

--- basicwrite.py
+++ basicwrite.py
@@ -19,28 +19,14 @@ class DesktopFile:
     # Set the output directory to ./Output/ for now, change it to .local/share
     # /applications later
     output_dir = 'Output/'
-    def __init__(self, filename, from_old=False):
+    def __init__(self):
         self.entry_dict = {}
-        if from_old:
-            self.old = open(filename, 'r')
-            self.populate_keys()
     def edit_key(self, key, value):
         if not key in self.valid_keys:
             print('Not a valid key type!')
         self.entry_dict[key] = value
     def find_current_key(self, key):
         pass
-    def populate_keys(self):
-        """
-        Add all the existing key/value pairs to the entry_dict
-        """
-        # List comprehension to find all lines containing a key=value pair
-        key_lines = [line for line in self.old if self.key_line.match(line)]
-        for line in key_lines:
-            split_line = line.split('=')
-            current_key = split_line[0]
-            current_val = split_line[1].rstrip()
-            self.entry_dict[current_key] = current_val
     def write_desk_file(self, out_filename):
         # This is the order of keys on the freedesktop.org example, seems like
         # a good standard to stick to for now
@@ -56,6 +42,22 @@ class DesktopFile:
             else:
                 # Check for the most essential keys here, e.g. Name, Exec
                 pass
+
+class ExistingDesktop(DesktopFile):
+    def __init__(self, filename):
+        self.entry_dict = {}
+        self.populate_keys()
+    def populate_keys(self):
+        """
+        Add all the existing key/value pairs to the entry_dict
+        """
+        # List comprehension to find all lines containing a key=value pair
+        key_lines = [line for line in self.old if self.key_line.match(line)]
+        for line in key_lines:
+            split_line = line.split('=')
+            current_key = split_line[0]
+            current_val = split_line[1].rstrip()
+            self.entry_dict[current_key] = current_val
 
 if __name__ == '__main__':
     des = DesktopFile('desuratest.desktop', from_old=True)
